ec45dcc04040d176faf2012bb871b07e2906be93
josecaos <jazzvoon@gmail.com>
2016-03-28 02:30:11 -0600

    borrado carpetas sobrantes

--- js/josecaos.js
+++
@@ -1,9 +0,0 @@
-//abre contacto
-jQuery('.widget_custom_cf7 h4.widgettitle').on('click', function() {
-  alert('sdfasdfa');
-  jQuery('.widget_custom_cf7 div.clearfix').slideToggle('slow');
-});
-//ready
-jQuery(function() {
-  alert('document ready');
-})

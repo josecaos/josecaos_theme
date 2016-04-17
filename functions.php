fcae557f3ed1c75200e1a37a917d7be193b05544
josecaos <jazzvoon@gmail.com>
2016-04-17 02:40:04 -0500

    leve fixes

--- functions.php
+++ functions.php
@@ -1,18 +1,33 @@
 <?php
 function theme_enqueue_styles() {
 
-    $parent_style = 'parent-style';
+  $parent_style = 'parent-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style )
-    );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'child-style',
+  get_stylesheet_directory_uri() . '/style.css',
+  array( $parent_style )
+);
+##custom scss compilado
+wp_enqueue_style( 'caos-style',
+get_stylesheet_directory_uri() . '/assets/css/josecaos.css',
+array($parent_style)
+);
+##fuente 8bit
+wp_enqueue_style( '8bit-font',
+'https://fonts.googleapis.com/css?family=Press+Start+2P',
+array($parent_style)
+);
+##fuente 8bit
+// wp_enqueue_style( 'cont-font',
+// 'https://fonts.googleapis.com/css?family=Press+Start+2P',
+// array($parent_style)
+// );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 //
 ##custom scripts
 function custom_scripts() {
-    wp_enqueue_script( 'josecaos-js', '/wp-content/themes/josecaos_theme/assets/js/josecaos.js', array( 'jquery' ), '0.1', true );
+  wp_enqueue_script( 'josecaos-js', get_stylesheet_directory_uri() . '/assets/js/josecaos.js', array( 'jquery' ), '0.1', true );
 }
 add_action('wp_enqueue_scripts', 'custom_scripts', 998);

930b4451ed427970fae67ebb277fcaedad1b4528
josecaos <jazzvoon@gmail.com>
2017-02-16 02:39:35 -0600

    avances header

--- header.php
+++ header.php
@@ -1,60 +1,88 @@
 <!doctype html>
 
-  <html class="no-js"  <?php language_attributes(); ?>>
+<html class="no-js"  <?php language_attributes(); ?>>
 
-	<head>
-		<meta charset="utf-8">
+<head>
+  <meta charset="utf-8">
 
-		<!-- Force IE to use the latest rendering engine available -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Force IE to use the latest rendering engine available -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<!-- Mobile Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta class="foundation-mq">
+  <!-- Mobile Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta class="foundation-mq">
 
-		<!-- If Site Icon isn't set in customizer -->
-		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
-			<!-- Icons & Favicons -->
-			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />
-			<!--[if IE]>
-				<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-			<![endif]-->
-			<meta name="msapplication-TileColor" content="#f01d4f">
-			<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
-	    	<meta name="theme-color" content="#121212">
-	    <?php } ?>
+  <!-- If Site Icon isn't set in customizer -->
+  <?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
+    <!-- Icons & Favicons -->
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />
+    <!--[if IE]>
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+    <![endif]-->
+    <meta name="msapplication-TileColor" content="#f01d4f">
+    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
+    <meta name="theme-color" content="#121212">
+    <?php } ?>
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
-		<!-- Analytics -->
-<script>
-  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  //
-  // ga('create', 'UA-74096683-1', 'auto');
-  // ga('send', 'pageview');
+    <!-- Analytics -->
+    <script>
+    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    //
+    // ga('create', 'UA-74096683-1', 'auto');
+    // ga('send', 'pageview');
 
-</script>
-		<!-- -->
+    </script>
+    <!-- -->
 
-	</head>
+  </head>
 
-	<body>
-					<header id="header-escritorio" class="columns h-10-v fixed top left p-0 hide-for-small">
+  <body>
+    <header id="header-escritorio" class="columns h-15-v fixed top left p-0 hide-for-small-only">
 
-            <i class="fa fa-bitcoin font-xxl black"></i>
+      <i class="fa fa-bitcoin font-xxl black"></i>
 
-					</header>
-					<header id="header-movil" class="columns h-10-v fixed top left p-0 show-for-small">
+    </header>
+    <header id="header-movil" class="columns h-10-v fixed top left p-0 show-for-small-only">
 
-            <i class="fa fa-microchip font-xxl black"></i>
+      <div class="columns small-10 medium-11">
 
-					</header>
+        <div class="row small-12 align-middle medium-1">
+          <div class="columns h-a">
+            Logo
+          </div>
+        </div>
+
+      </div>
+
+      <!--  -->
+      <div class="columns small-2 medium-11">
+        <div class="row small-12 align-middle medium-1">
+
+          <div class="iconos columns small-10 medium-11 font-l text-center h-a">
+            <!-- icono desde js -->
+          </div>
 
 
-          <div id="main" class="columns p-0">
+        </div>
+      </div>
+
+
+    </header>
+
+
+    <div id="main" class="columns p-0 p-top">
+
+
+
+
+      <div class="numeros columns">
+
+      </div>
--- js/josecaos.js
+++ js/josecaos.js
@@ -3,10 +3,12 @@ Funciones fuera del ready usar prefijo 'jQuery'
 en vez de '$', este solo dentro de el ready
 */
 jQuery(document).foundation()
-
+//
 jQuery(document).ready(function($){
 
   contador()
+
+  boton_menu_movil()
 
 })//ready
 
@@ -16,7 +18,8 @@ function contador() {
 
   setInterval(function(){
 
-    jQuery('#main').html('<p class="font-xxl">' + iter + '</p>')
+    jQuery('.numeros').html('<p class="font-xxl">' + iter + '</p>')
+
 
     if (iter <= 10) {
       iter = iter + 1
@@ -28,5 +31,32 @@ function contador() {
 
 
   },100)
+
+}
+
+function boton_menu_movil() {
+
+// cambia consecutivamente el icono del menu
+  var iconos = ['bars','barcode','align-left','barcode','align-right','barcode','align-justify','barcode','align-left','barcode','align-right','barcode']
+  var iconindex = 0
+  var arrlong = iconos.length - 2
+
+  setInterval(function(){
+
+    jQuery('.iconos').html('<a class="columns p-0"><i class="fa fa-' + iconos[iconindex] + ' "></i></a>')
+
+
+    if (iconindex <= arrlong)  {
+
+      iconindex = iconindex + 1
+
+    } else {
+
+      iconindex = 0
+
+    }
+
+
+  },125)
 
 }

f961269a0004ccb85441a64a7831d9359f3bba63
josecaos <jazzvoon@gmail.com>
2017-02-17 14:09:50 -0600

    fixes footer

--- footer.php
+++ footer.php
@@ -1,36 +1,34 @@
 </div>  <!-- end .main-content -->
 
-					<footer class="footer">
-						<div id="inner-footer" class="row h-100">
-							<div class="large-8 medium-12 columns">
+					<footer class="footer columns">
+						<div id="inner-footer" class="row h-100 align-middle">
 
-								<nav role="navigation" class="h_100">
+								<nav role="navigation" class="columns small-8 h-a font-s font-md-l text-center color-negro">
 
 										<ul id="redes_sociales" class="small-12 columns h_100 m0">
-											<a href="http://www.github.com/josecaos" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="http://www.github.com/josecaos" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-github"></i></li>
 											</a>
-											<a href="http://www.twitter.com/joseCao5" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="http://www.twitter.com/joseCao5" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-twitter"></i></li>
 											</a>
-											<a href="http://www.instagram.com/josecaos" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="http://www.instagram.com/josecaos" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-instagram"></i></li>
 											</a>
-											<a href="http://www.soundcloud.com/josecaos" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="http://www.soundcloud.com/josecaos" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-soundcloud"></i></li>
 											</a>
-											<a href="https://vimeo.com/mixfuckedup" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="https://vimeo.com/mixfuckedup" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-vimeo"></i></li>
 											</a>
-											<a href="https://www.youtube.com/user/mixfuckedupmac" target="_blank" class="small-2 columns text-center fontRM color_negro">
+											<a href="https://www.youtube.com/user/mixfuckedupmac" target="_blank" class="small-2 columns">
 												<li class="p2"><i class="fa fa-youtube"></i></li>
 											</a>
 										</ul>
 		    					</nav>
-		    				</div>
 
-							<div class="large-4 medium-12 columns h_100 align-middle pt1">
-								<div class="text-center source-org copyright align-center m0 p2">&copy;  <?php bloginfo('name'); ?> | <?php echo date('Y'); ?></div>
+							<div class="small-4 columns h_100 align-middle pt1">
+								<div class="text-center source-org copyright align-center m0 p2 font-xs font-md-m">&copy;  <?php echo date('Y');?> &nbsp; <?php  bloginfo('name'); ?></div>
 							</div>
 
 						</div> <!-- end #inner-footer -->

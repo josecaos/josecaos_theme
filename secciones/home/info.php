a5de45d37c3a22437ac6c739271444157ba92361
josecaos <jazzvoon@gmail.com>
2017-04-02 19:36:10 -0500

    fix sidebar

--- secciones/home/info.php
+++ secciones/home/info.php
@@ -9,8 +9,8 @@
 
     if ( have_posts() ) :
       while ( have_posts() ) : the_post(); ?>
-      <h2 class="columns p-0 text-center">Nuevo Release</h2>
-      <h3 class="columns p-0 text-center"><?php echo get_the_title(); ?></h3>
+      <h2 class="columns p-0 text-center text-shadow">Nuevo Release</h2>
+      <h3 class="columns p-0 text-center text-shadow"><?php echo get_the_title(); ?></h3>
 
       <div class="columns small-11 medium-10 large-8 small-centered texto-skewed h-a font-s font-sm-m font-lg-l">
 
--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-<sidebar class="columns small-12 medium-4 large-3 h-80-v text-center">
+<sidebar class="columns small-12 medium-4 large-3 h-80-v text-center cortina-rojo-bg p-t-0-3 p-b-0-3">
 
 
 
-  <div class="columns h-a text-center">
+  <div class="columns h-a text-left p-t-0-2 p-b-0-2">
     <!-- contador aqui desde js -->
 	<?php if ( is_active_sidebar( 'caos-1-sidebar' ) ) : ?>
 

a8b6488e2d1a20d141445d9c52b835635c1c33c6
josecaos <jazzvoon@gmail.com>
2017-08-06 00:56:00 -0500

    cambios al tema

---
+++ xibalba.php
@@ -0,0 +1,16 @@
+<?php
+/*Template Name: Xibalba-released*/
+
+get_header();
+
+?>
+
+<?php
+// get_template_part('secciones/home/heroscreen');
+get_template_part('secciones/released/xibalba-release');
+?>
+
+
+<?php
+
+get_footer();

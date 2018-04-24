69ad39044f97d08c7307d40ee52f18525a6db065
josecaos <jazzvoon@gmail.com>
2018-04-24 13:31:11 -0500

    Fixes para migrar sitio en 1era fase

--- secciones/fechas/fechas-contenido.php
+++ secciones/fechas/fechas-contenido.php
@@ -1,5 +1,7 @@
 <section class="columns small-12 medium-9 large-10 p-top">
 
+<h3 class="grid-x color-claro"><?php echo get_the_title();?></h3>
+
 <?php echo do_shortcode('[events age=all]');?>
 
 </section>
